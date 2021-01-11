89e4d4db55e36b550316bb585bd669194aeff2a5
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-01-11 15:02:58 +0100

    pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tableau-de-bord/' : '/',
+  publicPath: '/tableau-de-bord/',
   css: {
     loaderOptions: {
       sass: {
