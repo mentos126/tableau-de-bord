0f3c6de690e3093598721fb309d8be73ecd1f138
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-02-03 15:01:32 +0100

    Deploy to gh-pages.

---
+++ resources/Home.d082939b.js
@@ -0,0 +1,1 @@
+import{d as a,p as s,a as e,o,c as t,w as d,b as r}from"./index.6ee94673.js";var b=a({name:"Home",setup:()=>({})});const c=d("data-v-159b413b");s("data-v-159b413b");const n={class:"home"},l=r("img",{alt:"Vue logo",src:"./resources/logo.3b714202.png"},null,-1);e();const m=c(((a,s,e,d,r,b)=>(o(),t("div",n,[l]))));b.render=m,b.__scopeId="data-v-159b413b";export default b;
