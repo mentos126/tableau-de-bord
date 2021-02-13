8946d615f8daeaed43c4931c3c1c25a16cb030ce
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-02-13 14:09:55 +0100

    Deploy to gh-pages.

---
+++ assets/store.7fb70ba1.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index.d3723f0a.js";const e=function(r){return(new DOMParser).parseFromString(r,"text/html")},t=function(r,e){r.querySelectorAll(e).forEach((r=>{r.parentNode.removeChild(r)}))};function n(){return r}export{t as r,e as s,n as u};
